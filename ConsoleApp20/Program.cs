ebecf42846116555b6d0fc7c20c684cfb8def0eb
Mahbegum <mahbegum25@gmail.com>
2023-07-02 21:49:03 -0700

    Add project files.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inside of the Main method, instantiate and initialize an Employee object with
+            //the first name "Sample" and the last name "Student" 
+            employee employee = new employee() { FirstName = "Ali", LastName = "Samim"  };
+            employee sam = employee;
+            //Call the superclass method SayName() on the Employee object
+            sam.SayName();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp20/person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class person
+    {
+        //Create a class called Person and give it tow properties, each of data type string
+        // One called FirstName, the other LastName
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+      
+
+
+        //Give the class a void method called SayName() that take no paratmeters and simples writes
+        // the person's full name to the console in the format of: "Name: [full name]'
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + this.FirstName + "" + this.LastName);
+
+        }
+    }
+}
